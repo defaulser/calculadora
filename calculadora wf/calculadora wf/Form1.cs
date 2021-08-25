2a28480bf226ac801fe02a6b98568fce50bdddd9
defaulser <82618227+defaulser@users.noreply.github.com>
2021-08-25 20:33:24 -0300

    Add files via upload

---
+++ calculadora wf/calculadora wf/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculadora_wf
+{
+    public partial class Form1 : Form
+    {
+        decimal valor1 = 0, valor2 = 0;
+
+        string operacao = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            textResultado.Text = textResultado.Text + "0";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textResultado.Text = textResultado.Text + "1";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textResultado.Text = textResultado.Text + "2";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textResultado.Text = textResultado.Text + "3";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textResultado.Text = textResultado.Text + "4";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textResultado.Text = textResultado.Text + "5";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textResultado.Text = textResultado.Text + "6";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textResultado.Text = textResultado.Text + "7";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textResultado.Text = textResultado.Text + "8";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textResultado.Text = textResultado.Text + "9";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textResultado.Text = textResultado.Text + ".";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            valor2 = decimal.Parse(textResultado.Text, CultureInfo.InvariantCulture);
+
+            if(operacao == "SOMA")
+                {
+                textResultado.Text = Convert.ToString(valor1 + valor2);
+            }
+            else if (operacao == "SUB")
+                { 
+                textResultado.Text = Convert.ToString(valor1 - valor2);
+            }
+            else if (operacao == "MULT")
+                { 
+                textResultado.Text = Convert.ToString(valor1 * valor2);
+            }
+            else                
+            {
+                textResultado.Text = Convert.ToString(valor1 / valor2);
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textResultado.Text = "";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(textResultado.Text, CultureInfo.InvariantCulture);
+            textResultado.Text = "";
+            operacao = "SUB";
+            lblOperacao.Text = "-";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(textResultado.Text, CultureInfo.InvariantCulture);
+            textResultado.Text = "";
+            operacao = "MULT";
+            lblOperacao.Text = "X";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(textResultado.Text, CultureInfo.InvariantCulture);
+            textResultado.Text = "";
+            operacao = "DIV";
+            lblOperacao.Text = "/";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textResultado.Text = "";
+            valor1 = 0;
+            valor2 = 0;
+            lblOperacao.Text = "" ;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textResultado.Text != "")
+            {
+                valor1 = decimal.Parse(textResultado.Text, CultureInfo.InvariantCulture);
+                textResultado.Text = "";
+                operacao = "SOMA";
+                lblOperacao.Text = "+";
+            }
+        }
+    }
+}
